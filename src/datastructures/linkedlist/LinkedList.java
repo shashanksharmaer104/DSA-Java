14662df60d7bfe1b520e6e7ec18df6608ca1c2ed
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-06-19 18:39:49 +0530

    Added supporting print methods and append method

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -23,4 +23,38 @@ public class LinkedList {
         length = 1;
     }
 
+    // Supporting Methods
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    // Append at the last
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
 }
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList linkedListOne = new LinkedList(4);
+        linkedListOne.append(5);
+        linkedListOne.append(6);
+
+        linkedListOne.getHead();
+        linkedListOne.getTail();
+        linkedListOne.getLength();
+
+        linkedListOne.printList();
+
     }
 }
