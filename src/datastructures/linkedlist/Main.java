a7c4f4d781bb9c1949be18bc7a187e7dac64521c
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-06-21 15:46:37 +0530

    Added get and set method

--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -4,13 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList linkedListOne = new LinkedList(1);
-        //linkedListOne.append(2);
-        linkedListOne.removeFirst();
+        linkedListOne.append(2);
+        linkedListOne.append(3);
+        linkedListOne.append(4);
 
         linkedListOne.getHead();
         linkedListOne.getTail();
         linkedListOne.getLength();
 
+        //linkedListOne.printList();
+
+        System.out.println("Value at index 1: " + linkedListOne.get(0).value);
+
+        linkedListOne.set(1, 50);
         linkedListOne.printList();
 
     }
